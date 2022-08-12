07165638644074b34240e859b4f50583aa5f16a3
NorthCopibara <ElshinAleksandr@mail.ru>
2022-08-12 13:52:14 +0300

    Customization added

--- HomelessDude/Source/HomelessDude/Private/Game/View/Characters/HDBaseCharacter.cpp
+++ HomelessDude/Source/HomelessDude/Private/Game/View/Characters/HDBaseCharacter.cpp
@@ -8,3 +8,46 @@ AHDBaseCharacter::AHDBaseCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 }
+
+void AHDBaseCharacter::UpdateMeshes(TArray<FCustomizationConfig> Configs)
+{
+	for (auto CustomizationElement : CustomizationElements)
+	{
+		CustomizationElement.Value.Meshes.Empty();
+
+		for (auto MeshName : CustomizationElement.Value.MeshComponentsNames)
+		{
+			ChangeMeshVisibility(*MeshName, false);
+		}
+	}
+
+	for (const auto Config : Configs)
+	{
+		if (CustomizationElements.Contains(Config.CustomizationElement))
+		{
+			const auto CustomizationElement = CustomizationElements[Config.CustomizationElement];
+			if (CustomizationElement.MeshComponentsNames.IsValidIndex(Config.ActiveElement))
+			{
+				ChangeMeshVisibility(*CustomizationElement.MeshComponentsNames[Config.ActiveElement],
+				                     !CustomizationElement.MeshComponentsNames[Config.ActiveElement].IsEmpty());
+			}
+			else
+			{
+				if (CustomizationElement.MeshComponentsNames.IsValidIndex(CustomizationElement.DefaultElement))
+				{
+					ChangeMeshVisibility(*CustomizationElement.MeshComponentsNames[CustomizationElement.DefaultElement],
+					                     !CustomizationElement.MeshComponentsNames[CustomizationElement.DefaultElement].
+					                     IsEmpty());
+				}
+			}
+		}
+	}
+}
+
+void AHDBaseCharacter::ChangeMeshVisibility(FName MeshName, bool IsVisible)
+{
+	const auto MeshComponent = Cast<USkeletalMeshComponent>(
+		GetDefaultSubobjectByName(MeshName));
+	if (!MeshComponent) return;
+	MeshComponent->SetVisibility(IsVisible);
+}
--- HomelessDude/Source/HomelessDude/Private/Game/View/Characters/HDBaseCharacter.h
+++ HomelessDude/Source/HomelessDude/Private/Game/View/Characters/HDBaseCharacter.h
@@ -6,6 +6,44 @@
 #include "Character/ALSCharacter.h"
 #include "HDBaseCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class ECustomizationElement : uint8
+{
+	Hair,
+	Head,
+	Beard,
+	Body,
+	Hands,
+	Legs
+};
+
+USTRUCT(BlueprintType)
+struct FCustomizationConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Customization")
+	ECustomizationElement CustomizationElement;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Customization")
+	int32 ActiveElement;
+};
+
+USTRUCT(BlueprintType)
+struct FCustomizationMeshes
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Customization")
+	int32 DefaultElement;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Customization")
+	TArray<FString> MeshComponentsNames;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Customization")
+	TArray<USkeletalMeshComponent*> Meshes;
+};
+
 UCLASS()
 class HOMELESSDUDE_API AHDBaseCharacter : public AALSCharacter
 {
@@ -13,4 +51,20 @@ class HOMELESSDUDE_API AHDBaseCharacter : public AALSCharacter
 
 public:
 	AHDBaseCharacter(const FObjectInitializer& ObjectInitializer);
+
+	void UpdateMeshes(TArray<FCustomizationConfig> Configs);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Customization")
+	TMap<ECustomizationElement, FCustomizationMeshes> CustomizationElements;
+
+	//Test
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TArray<FCustomizationConfig> TestConfig;
+
+	UFUNCTION(BlueprintCallable, Category = "Tests")
+	void TestUpdate() { UpdateMeshes(TestConfig); }
+
+private:
+	void ChangeMeshVisibility(FName MeshName, bool IsVisible);
 };
